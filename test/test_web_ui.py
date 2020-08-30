275debdece7f50738e5cbc51cb53d65cab64a822
Archer <archerckk@163.com>
2020-08-30 23:49:52 +0800

    提交环境获取代码

--- test/test_web_ui.py
+++ test/test_web_ui.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
 import os
-
+import  configparser
+import sys
 
 def get_config():
-    pass
-
+    config=configparser.ConfigParser()
+    if sys.platform=='win32':
+        os.environ['HOMEPATH']='C:/test'
+        config.read(os.path.join(os.environ['HOMEPATH'],'selenium.ini'))
+    else:
+        config.read(os.path.join(os.environ['HOME'],'selenium.ini'))
+    print(config)
+    return config
 class TestWeb:
 
 
